506ad4e45444e85e20a0d9e090611962fc250c91
Grigore Stefan <g_stefan@yahoo.com>
2020-08-31 12:53:36 +0300

    + build config

--- build.cmd
+++ build.cmd
@@ -3,8 +3,10 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
+call build.config.cmd
+
 set ACTION=%1
 if "%1" == "" set ACTION=make
-echo -^> %ACTION% xui
+echo -^> %ACTION% %PROJECT%
 
 call build.%ACTION%.cmd
---
+++ build.config.cmd
@@ -0,0 +1,7 @@
+@echo off
+rem Public domain
+rem http://unlicense.org/
+rem Created by Grigore Stefan <g_stefan@yahoo.com>
+
+set PROJECT=xui
+set VERSION_LAST=1.0.0
