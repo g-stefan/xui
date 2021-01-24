c36598513f7c258ae465d77faf9ed4d46b56b9f4
Grigore Stefan <g_stefan@yahoo.com>
2021-01-24 10:13:33 +0200

    template refactor

--- source/template/template.js
+++ source/template/template.js
@@ -1,4 +1,4 @@
-/*
+/*!
 //
 // Copyright (c) 2020-2021 Grigore Stefan <g_stefan@yahoo.com>
 // Created by Grigore Stefan <g_stefan@yahoo.com>
@@ -7,111 +7,112 @@
 //
 */
 
-XUI.Template={};
+XUI.Template = {};
 
-(function(){
+XUI.Template.submenuScrollBars_ = null;
 
-	var this_=this;
-	var submenuScrollBars_=null;
+/**
+ * Initialization
+ */
+XUI.Template.init = function () {
+	$(".xui.-overlay-scrollbars").overlayScrollbars({ scrollbars: { clickScrolling: true } });
+	$("#navigation-drawer-content").overlayScrollbars({ scrollbars: { clickScrolling: true }, clipAlways: false });
+	XUI.Dashboard.notifyStateChange = function () {
+		var state = this.getState();
+		var scrollBars = $("#navigation-drawer-content").overlayScrollbars();
+		if (scrollBars) {
+			if (((state.mode == "normal") || (state.mode == "mini")) && (state.state == "closed")) {
+				$(".xui.navigation-drawer ul.xui.menu>li").each(function () {
+					if (this.classList.contains("-on")) {
+						this.classList.remove("-on");
+						this.classList.add("-was-on");
+					};
+					if (this.classList.contains("-open")) {
+						this.classList.remove("-open");
+					};
+					if (this.classList.contains("-overlay-scrollbars-active")) {
+						this.classList.remove("-overlay-scrollbars-active");
+						$(this).children("ul").overlayScrollbars().destroy();
+					};
+				});
+				setTimeout(function () {
+					scrollBars.sleep();
+				}, 50);
+			} else {
+				$(".xui.navigation-drawer ul.xui.menu>li").each(function () {
+					if (this.classList.contains("-was-on")) {
+						this.classList.remove("-was-on");
+						this.classList.add("-on");
+					};
+				});
+				$("#navigation-drawer-content ul>li.xui._submenu>ul").each(function () {
+					this.style.height = "auto";
+				});
+				scrollBars.update(true);
+			};
+		};
+	};
+	XUI.Dashboard.notifyStateChange();
+	$(".xui.navigation-drawer ul.xui.menu>li").mouseenter(function () {
+		var this_ = this;
+		$("ul>li.xui._submenu").each(function () {
+			if (this_ == this) {
+				return;
+			};
+			if (this.classList.contains("-open")) {
+				this.classList.remove("-open");
+			};
+			if (this.classList.contains("-overlay-scrollbars-active")) {
+				this.classList.remove("-overlay-scrollbars-active");
+				$(this).children("ul").overlayScrollbars().destroy();
+			};
+		});
+		if (this.classList.contains("_submenu")) {
+			this.classList.add("-open");
+			XUI.Capture.set([this], function (e, elList) {
+				elList[0].classList.remove("-open");
+				if (elList[0].classList.contains("-overlay-scrollbars-active")) {
+					elList[0].classList.remove("-overlay-scrollbars-active");
+					$(elList[0]).children("ul").overlayScrollbars().destroy();
+				};
+			});
+		};
+		if (!this.classList.contains("-overlay-scrollbars-active")) {
+			var el = this.getElementsByTagName("ul");
+			if (el.length) {
+				el[0].style.height = "auto";
+				var state = XUI.Dashboard.getState();
+				if (((state.mode == "normal") || (state.mode == "mini")) && (state.state == "closed")) {
+					var checkHeight = function () {
+						var elRect = el[0].getBoundingClientRect();
+						var viewRect = document.body.getBoundingClientRect();
+						var elViewHeight = elRect.top + elRect.height;
 
-	this.init=function(){
-		$(".xui.-overlay-scrollbars").overlayScrollbars({scrollbars:{clickScrolling:true}});
-		$("#navigation-drawer-content").overlayScrollbars({scrollbars:{clickScrolling:true},clipAlways:false});
-		XUI.Dashboard.notifyStateChange=function() {
-			var state=this.getState();
-			var scrollBars=$("#navigation-drawer-content").overlayScrollbars();
-			if(scrollBars){
-				if(((state.mode=="normal")||(state.mode=="mini"))&&(state.state=="closed")){
-					$(".xui.navigation-drawer ul.xui.menu>li").each(function(){
-						if(this.classList.contains("-on")){
-							this.classList.remove("-on");
-							this.classList.add("-was-on");
+						if (elViewHeight > viewRect.height) {
+							elNewHeight = viewRect.height - elRect.top - 6; // 6px margin bottom							
+							if (!this_.classList.contains("-overlay-scrollbars-active")) {
+								this_.classList.add("-overlay-scrollbars-active");
+								$(this_).children("ul").overlayScrollbars({ scrollbars: { clickScrolling: true }, clipAlways: false });
+							};
+							el[0].style.height = elNewHeight + "px";
+							el[0].style.overflowY = "auto";
 						};
-						if(this.classList.contains("-open")){
-							this.classList.remove("-open");
-						};
-						if(this.classList.contains("-overlay-scrollbars-active")){
-							this.classList.remove("-overlay-scrollbars-active");
-							$(this).children("ul").overlayScrollbars().destroy();
-						};
-					});
-					setTimeout(function(){
-						scrollBars.sleep();
-					},50);
-				}else{
-					$(".xui.navigation-drawer ul.xui.menu>li").each(function(){
-						if(this.classList.contains("-was-on")){
-							this.classList.remove("-was-on");
-							this.classList.add("-on");
-						};
-					});
-					$("#navigation-drawer-content ul>li.xui._submenu>ul").each(function(){
-						this.style.height = "auto";
-					});
-					scrollBars.update(true);
+					};
+
+					setTimeout(checkHeight, 500);
+					setTimeout(checkHeight, 800);
 				};
 			};
 		};
-		XUI.Dashboard.notifyStateChange();
-		$(".xui.navigation-drawer ul.xui.menu>li").mouseenter(function() {
-			var this_=this;
-			$("ul>li.xui._submenu").each(function(){
-				if(this_==this) {
-					return;
-				};
-				if(this.classList.contains("-open")){
-					this.classList.remove("-open");
-				};
-				if(this.classList.contains("-overlay-scrollbars-active")){
-					this.classList.remove("-overlay-scrollbars-active");
-					$(this).children("ul").overlayScrollbars().destroy();
-				};
-			});
-			if(this.classList.contains("_submenu")){
-				this.classList.add("-open");
-				XUI.Capture.set([this],function(e,elList){
-					elList[0].classList.remove("-open");
-					if(elList[0].classList.contains("-overlay-scrollbars-active")){
-						elList[0].classList.remove("-overlay-scrollbars-active");
-						$(elList[0]).children("ul").overlayScrollbars().destroy();
-					};
-				});
-			};
-			if(!this.classList.contains("-overlay-scrollbars-active")) {
-				var el=this.getElementsByTagName("ul");
-				if(el.length){
-					el[0].style.height = "auto";
-					var state=XUI.Dashboard.getState();
-					if(((state.mode=="normal")||(state.mode=="mini"))&&(state.state=="closed")){
-						var checkHeight=function() {
-							var elRect=el[0].getBoundingClientRect();
-							var viewRect=document.body.getBoundingClientRect();
-							var elViewHeight=elRect.top+elRect.height;
-						
-							if(elViewHeight>viewRect.height) {
-								elNewHeight=viewRect.height-elRect.top-6; // 6px margin bottom							
-								if(!this_.classList.contains("-overlay-scrollbars-active")) {
-									this_.classList.add("-overlay-scrollbars-active");
-									$(this_).children("ul").overlayScrollbars({scrollbars:{clickScrolling:true},clipAlways:false});
-								};
-								el[0].style.height = elNewHeight+"px";
-								el[0].style.overflowY = "auto";
-							};
-						};
-					
-						setTimeout(checkHeight,500);
-						setTimeout(checkHeight,800);
-					};
-				};
-			};
-		});
-	};
+	});
+};
 
-	this.load=function(event){
-		window.removeEventListener("load", this_.load);
-		this_.init();
-	};
+/**
+ * Init on window load event
+ */
+XUI.Template.load = function (event) {
+	window.removeEventListener("load", XUI.Template.load);
+	XUI.Template.init();
+};
 
-	window.addEventListener("load", this.load);
-
-}).apply(XUI.Template);
+window.addEventListener("load", XUI.Template.load);
